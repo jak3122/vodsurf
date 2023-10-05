415176b54cf6dabeb085c1f60088c0f07d186e4c
Joe Ksiazek <hyzer.jak@gmail.com>
2023-10-05 15:42:07 -0400

    Fix scripts

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,7 @@ module.exports = {
   apps: [
     {
       name: "vodsurf",
-      script: "pnpm",
-      args: "start",
+      script: "npm run start",
       instances: 1,
       autorestart: true,
       watch: false,
